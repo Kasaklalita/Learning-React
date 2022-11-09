116aac324e617be0b75a4d5126b3b93f5314698a
Veniamin Polienko <kasaklalita@mail.ru>
2022-11-09 18:54:35 +0300

    Working with arrays

--- react-fund-course/src/App.js
+++ react-fund-course/src/App.js
@@ -3,14 +3,25 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState("Текст в инпуте");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JS", body: "Description" },
+    { id: 2, title: "JS 2", body: "Description" },
+    { id: 3, title: "JS 3", body: "Description" },
+  ]);
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Python", body: "Description" },
+    { id: 2, title: "Python 2", body: "Description" },
+    { id: 3, title: "Python 3", body: "Description" },
+  ]);
 
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: "JS", body: "Description" }} />
+      <PostList posts={posts} title="Список постов 1" />
+      <PostList posts={posts2} title="Список постов 2" />
     </div>
   );
 }
