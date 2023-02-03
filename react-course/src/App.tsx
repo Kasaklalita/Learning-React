be2aeec7afa193b5ad9ba3acd6d8d09323f30874
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-03 13:17:50 +0300

    Removing posts

--- react-course/src/App.tsx
+++ react-course/src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useRef, useState } from "react";
 import Counter from "./components/Counter";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import "./styles/App.css";
 
 function App() {
@@ -11,46 +10,19 @@ function App() {
     { id: 2, title: "JavaScript 2", body: "Description" },
     { id: 3, title: "JavaScript 3", body: "Description" },
   ]);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
 
-  const addNewPost = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    const newPost = {
-      id: Date.now(),
-      title,
-      body,
-    };
+  const createPost = (newPost: any) => {
     setPosts([...posts, newPost]);
-    setTitle("");
-    setBody("");
-    console.log(newPost);
+  };
+
+  const removePost = (post: any) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
   };
 
   return (
     <div className="App">
-      <form>
-        <MyInput
-          value={title}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setTitle(e.target.value)
-          }
-          type="text"
-          placeholder="Название поста"
-        />
-        <MyInput
-          value={body}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setBody(e.target.value)
-          }
-          type="text"
-          placeholder="Описание поста"
-        />
-        <MyButton type="submit" onClick={addNewPost}>
-          Создать пост
-        </MyButton>
-      </form>
-      <PostList posts={posts} title="Список постов" />
+      <PostForm create={createPost} />
+      <PostList posts={posts} title="Список постов" remove={removePost} />
     </div>
   );
 }
---
+++ react-course/src/components/PostForm.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import MyButton from "./UI/button/MyButton";
+import MyInput from "./UI/input/MyInput";
+
+export default function PostForm({ create }) {
+  const [post, setPost] = useState({ title: "", body: "" });
+
+  const addNewPost = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const newPost = {
+      ...post,
+      id: Date.now(),
+    };
+    create(newPost);
+    setPost({ title: "", body: "" });
+  };
+
+  return (
+    <form>
+      <MyInput
+        value={post.title}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setPost({ ...post, title: e.target.value })
+        }
+        type="text"
+        placeholder="Название поста"
+      />
+      <MyInput
+        value={post.body}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setPost({ ...post, body: e.target.value })
+        }
+        type="text"
+        placeholder="Описание поста"
+      />
+      <MyButton type="submit" onClick={addNewPost}>
+        Создать пост
+      </MyButton>
+    </form>
+  );
+}
