b753a914f9bc8a62efc164bd45729c9fa529abd0
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-03 13:37:45 +0300

    Added sorting

--- react-course/src/App.tsx
+++ react-course/src/App.tsx
@@ -2,14 +2,18 @@ import React, { useRef, useState } from "react";
 import Counter from "./components/Counter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
 import "./styles/App.css";
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "JavaScript", body: "Description" },
-    { id: 2, title: "JavaScript 2", body: "Description" },
-    { id: 3, title: "JavaScript 3", body: "Description" },
+    { id: 2, title: "Python", body: "asdfsadf" },
+    { id: 3, title: "aaa", body: "111" },
   ]);
+
+  const [selectedSort, setSelectedSort] = useState("");
 
   const createPost = (newPost: any) => {
     setPosts([...posts, newPost]);
@@ -19,9 +23,28 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const sortPosts = (sort: string) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr />
+      <div>
+        <MyInput placeholder="Поиск">
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка по"
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
+      </div>
+
       {posts.length !== 0 ? (
         <PostList posts={posts} title="Список постов" remove={removePost} />
       ) : (
