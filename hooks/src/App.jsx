d6461015ddc31a663a2ccda83a2e85d1ae895675
Veniamin Polienko <kasaklalita@mail.ru>
2023-03-11 17:31:46 +0300

    useScroll

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Hover from "./components/Hover";
+import List from "./components/List";
 import useInput from "./hooks/useInput";
 
 function App() {
-  const username = useInput("");
-  const password = useInput("");
-
   return (
     <div className="App">
-      <Hover />
+      {/* <Hover /> */}
+      <List />
     </div>
   );
 }
